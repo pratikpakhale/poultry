10d5426c17456592655756ef3ae6dcefbaba82bc
pratik pakhale <pratikpakhale20@gmail.com>
2025-03-21 03:29:55 +0530

    feat: add clerk auth

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import { BottomNav } from "@/components/bottom-nav";
 
 import { FlocksProvider } from "@/store/flocks";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,36 +16,38 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <title>Pakhale Poultry</title>
-        <link rel="manifest" href="/manifest.json"></link>
-      </head>
-      <body className={inter.className}>
-        <Suspense>
-          <FlocksProvider>
-            <main className="min-h-[100dvh] pb-16">{children}</main>
-          </FlocksProvider>
-          <BottomNav />
-        </Suspense>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <title>Pakhale Poultry</title>
+          <link rel="manifest" href="/manifest.json"></link>
+        </head>
+        <body className={inter.className}>
+          <Suspense>
+            <FlocksProvider>
+              <main className="min-h-[100dvh] pb-16">{children}</main>
+            </FlocksProvider>
+            <BottomNav />
+          </Suspense>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
