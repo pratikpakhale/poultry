54bf01baee8f0dc029e0e7ddf913bc276dfaeefe
pratik pakhale <pratikpakhale20@gmail.com>
2025-03-21 02:00:37 +0530

    build: docker

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://server:5000/api/:path*",
+      },
+    ];
   },
 };
 
